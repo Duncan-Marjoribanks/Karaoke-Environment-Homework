346f0975eb40fb35b59e98df3e10c12a6a325568
Duncan <marjoribanks.d@gmail.com>
2018-04-20 13:55:13 +0100

    added max population property to room class

--- room.rb
+++ room.rb
@@ -1,16 +1,18 @@
 class Room
 
-  attr_reader :name
+  attr_reader :name, :max_population
   attr_accessor :population, :song_playing
 
-  def initialize(name)
+  def initialize(name, max_population)
     @name = name
     @population = []
     @song_playing = song_playing
+    @max_population = max_population
   end
 
+
   def add_guest_to_room(guest)
-    population << guest
+    population << guest if population.count < max_population
   end
 
   def remove_guest_from_room(guest)
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -8,9 +8,13 @@ require_relative("../guest")
 class RoomTest < MiniTest::Test
 
   def setup
-    @room_1 = Room.new("Rock and Roll room")
+    @room_1 = Room.new("Rock and Roll room", 4)
     @song_1 = Song.new("Nevermind")
     @guest_1 = Guest.new("Barney")
+    @guest_2 = Guest.new("Timmy")
+    @guest_3 = Guest.new("Jane")
+    @guest_4 = Guest.new("Belinda")
+    @guest_5 = Guest.new("Norman")
   end
 
 
@@ -35,10 +39,20 @@ class RoomTest < MiniTest::Test
   end
 
 def test_can_remove_guest_from_room
-  room_2 = Room.new("Disco room")
+  room_2 = Room.new("Disco room", 4)
   room_2.add_guest_to_room(@guest_1)
   result = room_2.remove_guest_from_room(@guest_1)
   assert_equal(0, room_2.population.count)
+end
+
+def test_too_many_people
+  @room_1.add_guest_to_room(@guest_1)
+  @room_1.add_guest_to_room(@guest_2)
+  @room_1.add_guest_to_room(@guest_3)
+  @room_1.add_guest_to_room(@guest_4)
+  @room_1.add_guest_to_room(@guest_5)
+  result = @room_1.population.count
+  assert_equal(4, result)
 end
 
 end
