0e864e036bdaec943a337cf53ed691a40a4e40a4
Duncan <marjoribanks.d@gmail.com>
2018-04-20 17:27:30 +0100

    added a till property to the room class and added the function to add the cost a guest pays to create a total in till for the room

--- room.rb
+++ room.rb
@@ -1,28 +1,31 @@
 class Room
 
   attr_reader :name, :max_population, :cost
-  attr_accessor :population, :song_playing
+  attr_accessor :population, :song_playing, :till
 
-  def initialize(name, max_population, cost)
+  def initialize(name, max_population, cost, till)
     @name = name
     @population = []
     @song_playing = song_playing
     @max_population = max_population
     @cost = cost
+    @till = till
   end
 
+  def play_song(song)
+    song_playing = song
+  end
 
-  def add_guest_to_room(guest)
+  def add_guest_to_room(room, guest)
     population << guest if population.count < max_population && guest.wallet >= cost
     guest.wallet -= cost
+    room.till += cost
+    p "Woohoo!" if song_playing == guest.fav_song
   end
 
   def remove_guest_from_room(guest)
     population.delete(guest)
   end
 
-  def play_song(song)
-    song_playing = song
-  end
 
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -8,14 +8,14 @@ require_relative("../guest")
 class RoomTest < MiniTest::Test
 
   def setup
-    @room_1 = Room.new("Rock and Roll room", 4, 10)
+    @room_1 = Room.new("Rock and Roll room", 4, 10, 100)
     @song_1 = Song.new("Nevermind")
-    @guest_1 = Guest.new("Barney", 20)
-    @guest_2 = Guest.new("Timmy", 40)
-    @guest_3 = Guest.new("Jane", 2)
-    @guest_4 = Guest.new("Belinda", 30)
-    @guest_5 = Guest.new("Norman", 5)
-    @guest_6 = Guest.new("Suzanna", 1)
+    @guest_1 = Guest.new("Barney", 20, "Nevermind")
+    @guest_2 = Guest.new("Timmy", 40, "Welcome to the Jungle")
+    @guest_3 = Guest.new("Jane", 2, "Waterloo")
+    @guest_4 = Guest.new("Belinda", 30, "Smack my bitch up")
+    @guest_5 = Guest.new("Norman", 5, "Happy birthday")
+    @guest_6 = Guest.new("Suzanna", 1, "Nevermind")
   end
 
 
@@ -35,24 +35,27 @@ class RoomTest < MiniTest::Test
   end
 
   def test_can_add_guests_to_room
-    result = @room_1.add_guest_to_room(@guest_1)
+    result = @room_1.add_guest_to_room(@room_1, @guest_1)
     assert_equal(1, @room_1.population.count)
+    assert_equal(10, @guest_1.wallet)
+    assert_equal(110, @room_1.till)
   end
 
 def test_can_remove_guest_from_room
-  room_2 = Room.new("Disco room", 4, 10)
-  room_2.add_guest_to_room(@guest_1)
+  room_2 = Room.new("Disco room", 4, 10, 200)
+  room_2.add_guest_to_room(room_2, @guest_1)
   result = room_2.remove_guest_from_room(@guest_1)
   assert_equal(0, room_2.population.count)
 end
 
 def test_too_many_people
-  @room_1.add_guest_to_room(@guest_1)
-  @room_1.add_guest_to_room(@guest_2)
-  @room_1.add_guest_to_room(@guest_3)
-  @room_1.add_guest_to_room(@guest_4)
-  @room_1.add_guest_to_room(@guest_5)
-  @room_1.add_guest_to_room(@guest_6)
+  @room_1.play_song(@song_1)
+  @room_1.add_guest_to_room(@room_1, @guest_1)
+  @room_1.add_guest_to_room(@room_1, @guest_2)
+  @room_1.add_guest_to_room(@room_1, @guest_3)
+  @room_1.add_guest_to_room(@room_1, @guest_4)
+  @room_1.add_guest_to_room(@room_1, @guest_5)
+  @room_1.add_guest_to_room(@room_1, @guest_6)
   result = @room_1.population.count
   assert_equal(3, result)
 end
