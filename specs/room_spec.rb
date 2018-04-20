b3f10b8d7debf49567b0d61e88d9311f6c56ebba
Duncan <marjoribanks.d@gmail.com>
2018-04-20 12:25:41 +0100

    initial commit

---
+++ specs/room_spec.rb
@@ -0,0 +1,12 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../room")
+
+
+class RoomTest < MiniTest::Test
+
+def setup
+  
+end
+
+end
---
+++ specs/song_spec.rb
@@ -0,0 +1,12 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../song")
+
+
+class SongTest < MiniTest::Test
+
+def setup
+  
+end
+
+end
